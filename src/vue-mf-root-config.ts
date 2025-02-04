65685ad94e9e0804b97bc2319fd58d644a8f1dd6
Joel Denning <joeldenning@gmail.com>
2025-02-04 01:20:33 -0600

    Upgrade dependencies, migrate from SystemJS to native modules (#41)
    * Upgrade dependencies, migrate from SystemJS to native modules
    
    * Self review
    
    * Add github build
    
    * Specify packageManager

---
+++ src/vue-mf-root-config.ts
@@ -0,0 +1,20 @@
+import { registerApplication, start } from "single-spa";
+import {
+  constructApplications,
+  constructRoutes,
+  constructLayoutEngine,
+} from "single-spa-layout";
+import microfrontendLayout from "./microfrontend-layout.html";
+
+const routes = constructRoutes(microfrontendLayout);
+const applications = constructApplications({
+  routes,
+  loadApp({ name }) {
+    return import(/* webpackIgnore: true */ name);
+  },
+});
+const layoutEngine = constructLayoutEngine({ routes, applications });
+
+applications.forEach(registerApplication);
+layoutEngine.activate();
+start();
