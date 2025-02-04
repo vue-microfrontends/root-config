5fc523a8a801c68582cf01974bcd355437b96ca6
Joel Denning <joeldenning@gmail.com>
2025-02-04 00:13:44 -0700

    Upgrade dependencies, migrate from SystemJS to native modules

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import babelParser from "@babel/eslint-parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  ...compat.extends("ts-important-stuff", "plugin:prettier/recommended"),
+  {
+    files: ["**/*.js", "**/*.ts"],
+    languageOptions: {
+      parser: babelParser,
+    },
+  },
+];
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,31 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-ts");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = (env) => ({
-  entry: path.resolve(__dirname, "src/vue-mf-root-config"),
-  output: {
-    filename: "vue-mf-root-config.js",
-    libraryTarget: "system",
-    path: path.resolve(__dirname, "dist"),
-  },
-  devtool: "sourcemap",
-  module: {
-    rules: [
-      { parser: { system: false } },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [{ loader: "babel-loader" }],
-      },
+module.exports = (webpackConfigEnv, argv) => {
+  const orgName = "vue-mf";
+  const defaultConfig = singleSpaDefaults({
+    orgName,
+    projectName: "root-config",
+    webpackConfigEnv,
+    argv,
+    disableHtmlGeneration: true,
+  });
+
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+    // devServer: {
+    //   hot: false,
+    // },
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: "src/index.ejs",
+        templateParameters: {
+          isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
+          orgName,
+        },
+      }),
     ],
-  },
-  devServer: {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-    },
-    disableHostCheck: true,
-    historyApiFallback: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: false,
-      template: "src/index.ejs",
-      templateParameters: {
-        isLocal: env && env.isLocal === "true",
-      },
-    }),
-    new CleanWebpackPlugin(),
-  ],
-  externals: ["single-spa", "vue", "vue-router", /^@vue-mf\/.+$/],
-});
+  });
+};
